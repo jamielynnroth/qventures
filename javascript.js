0673c56cc36df57082f31044d0f91c69de1bcf38
jamielynnroth <roth.j@me.com>
2013-06-24 18:44:16 -0400

    add tint to news, add jquery to team and portfolio pages

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@
 	// Show and Hide Rollover state of MadeinNY heart
 		$(function() {
 			$('#madeinny').hover(function() {
-				console.log('poop');
+				// console.log('poop');
 				$('#whiteheart').removeClass('show');
 				$('#whiteheart').addClass('hide');
 				$('#hoverstate').removeClass('hide');
 				$('#hoverstate').addClass('show');
 			}, function() {
-				console.log('eeeee');
+				// console.log('eeeee');
 				$('#whiteheart').removeClass('hide');
 				$('#whiteheart').addClass('show');
 				$('#hoverstate').removeClass('show');
@@ -19,10 +19,34 @@
 
 		$(function() {
 			$('.pure-u-1-4').hover(function() {
-				// $(this).hide('img');
 				$(this).show('.rollover');
-			// }, function() {
-			// 	$(this).hide('.rollover');
-				// $(this).show('img');
+			});
+		});
+
+// Team page toggle show .rollover state on hover for small blocks
+		$(function () {
+			$('#teamgrid .pure-u-1-4').hover(function () {
+				$('.rollover', this).addClass('teamshow');
+			}, function () {
+				$('.rollover', this).removeClass('teamshow');
+			});
+		});
+		
+// Team page toggle .rollover state on hover for Pedro
+		$(function () {
+			$('#teamgrid .pure-u-1-2').hover(function () {
+				$('.rollover', this).addClass('teamshow');
+			}, function () {
+				$('.rollover', this).removeClass('teamshow');
+			});
+		});
+
+// Portfolio Page - toggle rollover state on hover 
+		$(function () {
+			$('#companygrid > .pure-u-1-4').hover(function() {
+				$('.rollover', this).addClass('teamshow');
+			}, function () {
+				$('.rollover', this).removeClass('teamshow');
+			
 			});
 		});
